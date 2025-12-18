c110366ae7917fa0122dc695fe5cef2272a10321
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-12-17 22:42:01 -0500

    updating header element to inside sticky

--- src/components/layout/SiteHeader.tsx
+++ src/components/layout/SiteHeader.tsx
@@ -6,8 +6,13 @@ export function SiteHeader() {
     <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4">
       <SidebarTrigger className="-ml-1" />
       <Separator orientation="vertical" className="mr-2 h-4" />
-      <div className="flex items-center gap-2">
-        <span className="font-semibold">Phillips Education</span>
+      <div className="flex flex-1 justify-center items-center gap-2">
+        <img
+          src="/assets/philips-corp-brand-mark.png"
+          alt="Phillips Logo"
+          className="h-8"
+        />
+        <h1 className="text-2xl font-bold italic">Phillips Education</h1>
       </div>
     </header>
   );
